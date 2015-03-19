b44678b2d54bca4a8134ec56ff61964f9f951597
Jan Waś <janek.jan@gmail.com>
2015-03-19 23:00:47 +0100

    added a test fixture

---
+++ tests/unit/fixtures/data/user.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    [
+        'name' => 'Laury',
+        'phone' => '285.924.2298x61982',
+        'city' => 'South Jeanneborough',
+        'password' => '$2y$13$C.PL.AlV3RlPbjAUm4R3xOjwsw4WlaHQ5aeVUGfiex6js.MaMKM.W',
+        'auth_key' => '-hykSg8w2gHw9oHz74aoayRCqj8owHqO',
+        'intro' => 'Doloribus nostrum illum iusto blanditiis voluptas quia mollitia autem aut.',
+    ],
+    [
+        'name' => 'Ruben',
+        'phone' => '284.610.1204',
+        'city' => 'Harrismouth',
+        'password' => '$2y$13$QVrf3goXb6SjwEIW6P64MOfcNkA71MDN7Z8myfXTHFe9x.UzQ65ba',
+        'auth_key' => 'CLX_DOZ8rvxzBskZEQUV6dPd6h2OQWi2',
+        'intro' => 'Reprehenderit nostrum et quo et.',
+    ],
+];
